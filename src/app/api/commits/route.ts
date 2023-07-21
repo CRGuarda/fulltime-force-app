b6eee53009951db52584262b1c3e5b38ac2fd613
CRGuarda <crguarda95@gmail.com>
2023-07-21 17:13:18 -0500

    default styles deleted, data fetching types and  first UI attempt

--- src/app/api/commits/route.ts
+++ src/app/api/commits/route.ts
@@ -1,1 +1,33 @@
-export async function GET(request: Request) {}
+import { Commit } from '@/app/interfaces/types.dto'
+import { NextResponse } from 'next/server'
+import { Octokit, App } from 'octokit'
+
+const octokit = new Octokit({ auth: process.env.API_TOKEN })
+
+export async function GET(request: Request) {
+  try {
+    const {
+      data: { login, avatar_url, html_url },
+    } = await octokit.rest.users.getAuthenticated()
+
+    const { data } = await octokit.request('GET /repos/CRGuarda/fulltime-force-app/commits', {
+      owner: 'CRGuarda',
+      repo: 'fulltime-force-app',
+      headers: {
+        'X-GitHub-Api-Version': '2022-11-28',
+      },
+    })
+
+    const commitResponse = data?.map(({ sha, commit, html_url }: { sha: string; commit: Commit; html_url: string }) => {
+      return { sha, commit, html_url }
+    })
+    return NextResponse.json({ login, avatar_url, html_url, commitResponse })
+  } catch (error) {
+    let errorMessage = 'Something bad happened.'
+    if (error instanceof Error) {
+      errorMessage = error.message
+      return NextResponse.json({ Error: error.message }, { status: 404 })
+    }
+    console.log(errorMessage)
+  }
+}
---
+++ src/helpers/getCommits.js
@@ -0,0 +1,11 @@
+import { headers } from 'next/headers'
+
+export async function getCommits() {
+  const host = headers().get('host')
+  const protocol = process.env.NODE_ENV !== 'development' ? 'https' : 'http'
+  const res = await fetch(`${protocol}://${host}/api/commits`)
+  if (!res.ok) {
+    throw new Error('Failed to fetch data')
+  }
+  return res.json()
+}
